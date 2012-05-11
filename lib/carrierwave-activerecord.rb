0348d58784b566f50ff17c8e8f1e982e2ca55e55
Richard Michael <rmichael@edgeofthenet.org>
2012-05-11 11:19:39 +0200

    Debugging.

--- lib/carrierwave-activerecord.rb
+++ lib/carrierwave-activerecord.rb
@@ -13,31 +13,41 @@ class CarrierWave::Uploader::Base
     config.active_record_tablename         = 'carrier_wave_files'
     config.active_record_nocache           = false
   end
+
+# def move_to_cache
+#   false
+# end
+
+# def copy_to_cache
+#   false
+# end
 end
 
 # FIXME: This feels like a horrible hack, but where else to inject the method
 # overriding into SanitizedFile?
 
-module Carrierwave
-  module Uploader
-    module Store
+# module Carrierwave
+#   module Uploader
+#     module Base
 
-      alias_method :original_store!, :store!
+#arrierWave::Uploader::Base.class_eval do
 
-      def store!
-        # FIXME: Needs a class_eval to do this in the Uploader instance.
-        if self.active_record_nocache?
-          ::CarrierWave::SanitizedFile.class_eval do
-            # Rails.logger.info "copy_to called by: #{caller[0...10]}"
-            # Rails.logger.info "move_to called by: #{caller[0...10]}"
-            def move_to(*args); self; end
-            def copy_to(*args); self; end
-          end
-        end
+#     alias_method :original_store!, :store!
 
-        original_store!
-      end # #store!
+#     def store!
+#       # FIXME: Needs a class_eval to do this in the Uploader instance.
+#       if self.active_record_nocache?
+#         ::CarrierWave::SanitizedFile.class_eval do
+#           # Rails.logger.info "copy_to called by: #{caller[0...10]}"
+#           # Rails.logger.info "move_to called by: #{caller[0...10]}"
+#           def move_to(*args); self; end
+#           def copy_to(*args); self; end
+#         end
+#       end
 
-    end # Store
-  end # Uploader
+#       original_store!
+#     end # #store!
+
+#   end # Store
+# end # Uploader
 end
