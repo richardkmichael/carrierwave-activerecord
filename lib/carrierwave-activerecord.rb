6b1cc8bd234b232736125363201bb42f3d1947ca
Julien Gantner <julien.gantner@softwareinmotion.de>
2012-09-21 13:44:29 +0200

    rearranges the code a bit

--- lib/carrierwave-activerecord.rb
+++ lib/carrierwave-activerecord.rb
@@ -2,28 +2,34 @@ require 'active_record'
 require 'carrierwave'
 require 'carrierwave-activerecord/version'
 
-CarrierWave::Storage.autoload :ActiveRecord, 'carrierwave-activerecord/storage/active_record'
-
-class CarrierWave::Uploader::Base
-  add_config :active_record_tablename
-  add_config :active_record_cache
-
-  configure do |config|
-    config.storage_engines[:active_record] = 'CarrierWave::Storage::ActiveRecord::StorageEngine'
-    config.active_record_tablename         = 'carrier_wave_files'
-    config.active_record_cache             = false
+module CarrierWave
+  module Storage
+    autoload :ActiveRecord, 'carrierwave-activerecord/storage/active_record'
   end
 
-  alias_method :original_cache!, :cache!
+  module Uploader
+    class Base
+      add_config :active_record_tablename
+      add_config :active_record_cache
 
-  def cache!(new_file)
-    unless active_record_cache
-      CarrierWave::SanitizedFile.class_eval do
-        def move_to(*args); self; end
-        def copy_to(*args); self; end
+      configure do |config|
+        config.storage_engines[:active_record] = 'CarrierWave::Storage::ActiveRecord::StorageEngine'
+        config.active_record_tablename         = 'carrier_wave_files'
+        config.active_record_cache             = false
+      end
+
+      alias_method :original_cache!, :cache!
+
+      def cache!(new_file)
+        unless active_record_cache
+          CarrierWave::SanitizedFile.class_eval do
+            def move_to(*args); self; end
+            def copy_to(*args); self; end
+          end
+        end
+
+        original_cache!(new_file)
       end
     end
-
-    original_cache!(new_file)
   end
 end
