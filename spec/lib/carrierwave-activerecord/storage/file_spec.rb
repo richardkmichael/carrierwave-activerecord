534dda33b20571cdcf14a4eabebc4fdb3f1eb9f1
Julien Gantner <julien.gantner@softwareinmotion.de>
2012-09-21 14:42:07 +0200

    splits the specs analog to the module into several files

---
+++ spec/lib/carrierwave-activerecord/storage/file_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+module CarrierWave 
+  module Storage
+    module ActiveRecord 
+      describe File do
+        it 'must be tested'
+      end
+    end
+  end # Storage
+end # CarrierWave
+
---
+++ spec/lib/carrierwave-activerecord/storage/storage_provider_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+module CarrierWave 
+  module Storage
+    module ActiveRecord 
+      describe StorageProvider do
+        class DummyUploader < CarrierWave::Uploader::Base
+          def store_path
+            "/uploads/bla.txt"
+          end
+        end
+
+        before :each do
+          @uploader = DummyUploader.new
+          @storage = StorageProvider.new(@uploader)
+          @file = double('file')
+          @initialization_values = { filename: 'sample.png', original_filename: 'o_sample.png', content_type: 'image/png', size: 123, extension: 'png', data: 1337 }
+          @initialization_values.each do |property, value|
+            @file.stub(property => value)
+          end
+          @file.stub(:read => 1337)
+        end
+
+        describe '#store!(file)' do
+          it 'calls FileProxy.create! with the given file and the storage path' do
+            FileProxy.should_receive(:create!).with(@file, @uploader.store_path)
+            @storage.store!(@file)
+          end
+
+          it 'returns the file wrapped in an instance of CarrierWave::ActiveRecord::FileProxy' do
+            @storage.store!(@file).should be_kind_of FileProxy
+          end
+
+          it 'fetches the storage path from the uploader' do
+            @uploader.should_receive(:store_path).with(no_args)
+            @storage.store!(@file)
+          end
+        end
+
+        describe '#retrieve!(identifier)' do
+          it 'calls FileProxy.fetch!(identifier)' do
+            FileProxy.should_receive(:fetch!).with('super_unique_identifier')
+            @storage.retrieve!('super_unique_identifier')
+          end
+
+          it 'returns an intance of CarrierWave::ActiveRecord::FileProxy' do
+            @storage.retrieve!('super_unique_identifier').should be_kind_of FileProxy
+          end
+        end
+      end
+    end
+  end
+end
