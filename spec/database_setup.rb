5931eac72b4e6011c96c19d496a459f3fd0f8d58
Richard Michael <rmichael@edgeofthenet.org>
2013-01-31 01:01:10 +0100

    Implementation and spec for File methods; minimal columns in the database.

--- spec/database_setup.rb
+++ spec/database_setup.rb
@@ -2,13 +2,12 @@ ActiveRecord::Base.establish_connection( adapter:  'sqlite3',
                                          database: ':memory:' )
 ActiveRecord::Schema.define do
   create_table :carrier_wave_files do |t|
+
+    t.string  :identifier
     t.string  :original_filename
     t.string  :content_type
-    t.string  :identifier
-    t.string  :extension
-    t.string  :filename
-    t.binary  :data
     t.integer :size
+    t.binary  :data
 
     t.timestamps
   end
